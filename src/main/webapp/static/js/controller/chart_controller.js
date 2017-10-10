7ecd20cb8a91fa8a869903d7bb44c7ca3d05e67a
JorgeHdez <jahm92@outlook.com>
2017-10-10 11:08:51 -0500

    grafica implementada

--- src/main/webapp/static/js/controller/chart_controller.js
+++ src/main/webapp/static/js/controller/chart_controller.js
@@ -19,19 +19,7 @@ App.controller('ChartController', ['$scope', 'HarvestService', function ($scope,
 			console.log($scope.listHarvest);
 
 			listaCose = $scope.listHarvest; 
-			// for (var n = 0; n < $scope.listHarvest.length; n++) {
-			// 	var i = 0;
-			// 	for (var key in $scope.listHarvest[n]) {
-			// 		console.log(n + "," + i + "->" + key + "=" + $scope.listHarvest[n][key])
-			// 		i++;
-			// 	}
-			// }
-			// google.charts.load('current', { 'packages': ['line'] });
-			// 
-
-
-			google.charts.load('current', { 'packages': ['line'] });
-			//google.charts.setOnLoadCallback(drawChartLine);
+			google.charts.load('current', { 'packages': ['line'], 'language': 'es'});
 			google.charts.setOnLoadCallback(cosecha);
 
 		},
@@ -45,18 +33,14 @@ App.controller('ChartController', ['$scope', 'HarvestService', function ($scope,
 
 		var data = new google.visualization.DataTable();
 		data.addColumn('date', 'Fecha');
-		data.addColumn('number', 'Precio');
-		data.addColumn('number', 'Peso');
+		data.addColumn('number', 'Precio ($)');
+		data.addColumn('number', 'Peso (kg.)');
 
 		data.addRows(listaCose.length);
 
 		for (var n = 0; n < listaCose.length; n++) {
 			var i = 0;
 			for (var key in listaCose[n]) { 	 	
-//				if(key === "idHarvest"){
-//					data.setValue(n, 0, listaCose[n][key]);
-//					console.log(n + "," + i + "->" + key + "=" + listaCose[n][key]);
-//				}
 				if(key === "date"){
 					var datee = moment(listaCose[n][key]);
 					console.log(datee);
@@ -65,90 +49,45 @@ App.controller('ChartController', ['$scope', 'HarvestService', function ($scope,
 					console.log('Year: '+datee.get('year'));
 					var dateJS = new Date(datee.year(), datee.get('month'), datee.get('date'));
 					console.log(dateJS);
-					//data.setValue(n, 0,   moment(listaCose[n][key]).format('YYYY-MM-DD'));
 					data.setValue(n, 0,   dateJS);
-					//console.log(n + "," + i + "->" + key + "=" + listaCose[n][key]);
-					
-					
 				}
 
 				if(key === "price"){
 					data.setValue(n, 1, listaCose[n][key]);
-					//console.log(n + "," + i + "->" + key + "=" + listaCose[n][key]);
 				}
 
 				if(key === "weight"){
 					data.setValue(n, 2, listaCose[n][key]);
 					console.log(n + "," + i + "->" + key + "=" + listaCose[n][key]);
 				}
-
-
-
-				//console.log(n + "," + i + "->" + key + "=" + listHarvest[n][key]);
 				i++;
 			}
 		}
 
+		var formatter_long = new google.visualization.DateFormat({formatType: 'long'});
+		
 		var options = {
 			chart: {
-				title: 'Box Office Earnings in First Two Weeks of Opening',
-				subtitle: 'in millions of dollars (USD)'
+				title: 'Grafica de produccion y venta por periodo',
+				subtitle: 'Producciom Gitomate'
 			},
 			width: 900,
-			height: 500
+			height: 500,
+			 hAxis: {
+				 title: "Fecha",
+                 gridlines: { count: 3, color: '#CCC' },
+                 format: 'dd MMM yyyy'
+		          },
+			vAxis: {
+				title: "Cantidad",
+				format: '###,###'
+			}
 		};
 
 		var chart = new google.charts.Line(document.getElementById('line_chart_div'));
-
 		chart.draw(data, google.charts.Line.convertOptions(options));
 
 	}
-	
-
-	function drawChartLine() {
-
-		
-
-		var data = new google.visualization.DataTable();
-		data.addColumn('number', 'Day');
-		data.addColumn('number', 'Precio');
-		data.addColumn('number', 'Peso');
-
-		data.addRows([
-			[1, 37.8, 80.8],
-			[2, 30.9, 69.5],
-			[3, 25.4, 57],
-			[4, 11.7, 18.8],
-			[5, 11.9, 17.6],
-			[6, 8.8, 13.6],
-			[7, 7.6, 12.3],
-			[8, 12.3, 29.2],
-			[9, 16.9, 42.9],
-			[10, 12.8, 30.9],
-			[11, 5.3, 7.9],
-			[12, 6.6, 8.4],
-			[13, 4.8, 6.3],
-			[14, 4.2, 6.2]
-		]);
-
-		var options = {
-			chart: {
-				title: 'Box Office Earnings in First Two Weeks of Opening',
-				subtitle: 'in millions of dollars (USD)'
-			},
-			width: 900,
-			height: 500
-		};
-
-		var chart = new google.charts.Line(document.getElementById('line_chart_div'));
-
-		chart.draw(data, google.charts.Line.convertOptions(options));
-
-
-
-		
-	}
-
 
 }]);
 
--- src/main/webapp/static/view/home.jsp
+++ src/main/webapp/static/view/home.jsp
@@ -1,7 +1,5 @@
 <div class="panel panel-default" ng-controller="ChartController as chartCtrl">
-	<div class="panel-body">Inicio</div>
-	
-	
+
 	<div id="line_chart_div" style="width: 900px; height: 500px;"></div>
 	
 </div>
